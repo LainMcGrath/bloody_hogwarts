d175ff67e0c5ab5d38b3df32ee48a65ed003b3bd
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-06 10:17:49 -0700

    Add user story 2

--- spec/feature/students/user_can_see_a_specific_student_rspec.rb
+++ spec/feature/students/user_can_see_a_specific_student_rspec.rb
@@ -5,6 +5,10 @@ RSpec.describe "User can see a specific students" do
     student_1 = Student.create( name: "Harry Potter",
                                 age: 18,
                                 house: "Gryffindor")
+
+    student_1_course = student_1.courses.create(name: "History of Magic")
+    student_1_course = student_1.courses.create(name: "Care of Magical Creatures")
+
     visit "/students/#{student_1.id}"
 
     expect(page).to have_content("History of Magic")
